9c640e6a629df9c7ef7e4d32ad3d73bf576528bc
inhaJoo <milk7821@inha.edu>
2023-09-26 11:32:04 +0900

    vue 초급강좌 last

---
+++ vue-test/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">ID : </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+    <div>
+      <app-header v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+import AppHeader from './components/HelloWorld.vue';
+
+export default {
+  data:function(){
+    return {
+      username: '',
+      password: '',
+      str : ''
+    }
+  },
+  components:{
+    'app-header':AppHeader
+  },
+  methods: {
+    submitForm : function(){
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username : this.username,
+        password : this.password 
+      }
+      axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+    },
+    renewStr:function() {
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-test/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+    <button v-on:click="passData">click</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props:['propsdata'],
+  methods:{
+    passData: function(){
+      this.$emit('renew');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
