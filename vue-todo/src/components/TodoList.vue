c12c69bf607ec1e23cd2547c9048fdf6550b7a25
inhaJoo <milk7821@inha.edu>
2023-10-05 11:20:08 +0900

    Todo App - 프로젝트구현

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,79 @@
+<template>
+  <ul>
+    <li v-for="(todoItem, index) in todoItems" class="shadow" v-bind:key="todoItem.item">
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem)">check</i>
+        <span>{{ todoItem.item }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+            <i class="removeBtn fas fa-trash-alt">remove</i>
+        </span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            todoItems:[]
+        }
+    },
+    methods:{
+        removeTodo:function(todoItem, index){
+            this.todoItems.splice(index, 1);//splice(시작인덱스, 적용갯수)
+            localStorage.removeItem(todoItem.item);
+        },
+        toggleComplete:function(todoItem){
+            console.log(todoItem);
+            todoItem.completed = !todoItem.completed;
+            //업데이트시 제거하고 다시 넣어주어야함
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+        }
+    },
+    created:function(){
+        if(localStorage.length > 0){
+            for(var i=0; i<localStorage.length; i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  /* color: black; */
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  /* color: #62acde; */
+  color: black;
+}
+.textCompleted {
+  text-decoration: line-through;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
