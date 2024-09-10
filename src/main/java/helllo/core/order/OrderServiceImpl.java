fbe2b9c4849a268c7e404364caedf1d95a897423
JHL <jhlee07@bandisnc.com>
2024-09-10 18:01:23 +0900

    9/10 스프링원리 orderServiceImpl

--- src/main/java/helllo/core/order/OrderServiceImpl.java
+++ src/main/java/helllo/core/order/OrderServiceImpl.java
@@ -2,13 +2,15 @@ package helllo.core.order;
 
 import helllo.core.discount.DiscountPolicy;
 import helllo.core.discount.FixDiscountPolicy;
+import helllo.core.discount.RateDiscountPolicy;
 import helllo.core.member.Member;
 import helllo.core.member.MemberRepository;
 import helllo.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
